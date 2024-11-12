c73603acae644fe05154b0161a9091a3dde9e1b8
C12H22O12 <director20844@gmail.com>
2024-11-12 16:36:42 +0900

    feat: 헤드 컴포넌트 작업

--- app/layouts/ResultLayout.tsx
+++ app/layouts/ResultLayout.tsx
@@ -1,8 +1,10 @@
+import Header from "@/components/Header";
 import { Text, View } from "react-native";
 
 const ResultLayout = () => {
   return (
     <View>
+      <Header title={"AI 분석 결과"} />
       <Text>ResultLayout</Text>
     </View>
   );
---
+++ components/Header.tsx
@@ -0,0 +1,72 @@
+import { Ionicons } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
+import styled from "styled-components/native";
+import { StyleProp, TextStyle, ViewStyle } from "react-native";
+import { Colors } from "@/constants/Colors";
+
+interface HeaderProps {
+  title: string;
+  showBackButton?: boolean;
+  containerStyle?: StyleProp<ViewStyle>;
+  titleStyle?: StyleProp<TextStyle>;
+}
+
+const Header = ({
+  title,
+  showBackButton = true,
+  containerStyle,
+  titleStyle,
+}: HeaderProps) => {
+  const navigation = useNavigation();
+
+  return (
+    <Container style={containerStyle}>
+      <LeftSection>
+        {showBackButton && (
+          <BackButton onPress={() => navigation.goBack()}>
+            <Ionicons
+              name="chevron-back"
+              size={24}
+              color={Colors.highlight.highlight_0}
+            />
+          </BackButton>
+        )}
+      </LeftSection>
+
+      <CenterSection>
+        <Title style={titleStyle}>{title}</Title>
+      </CenterSection>
+    </Container>
+  );
+};
+
+export default Header;
+
+const Container = styled.View`
+  height: 64px;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 10px;
+  border-bottom-width: 1px;
+  border-bottom-color: #eeeeee;
+`;
+
+const LeftSection = styled.View`
+  width: 50px;
+  align-items: flex-start;
+`;
+
+const CenterSection = styled.View`
+  flex: 1;
+  align-items: left;
+`;
+
+const BackButton = styled.TouchableOpacity`
+  padding: 8px;
+`;
+
+const Title = styled.Text`
+  font-size: 32px;
+  font-weight: 600;
+`;
