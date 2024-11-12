86a535429c62a60964ef37e894e9a198c5ef6fba
C12H22O12 <director20844@gmail.com>
2024-11-12 17:07:38 +0900

    feat: ResultLayout 틀 잡기, 텍스트만 있는 컴포넌트 OnlyText 작업

--- app/layouts/ResultLayout.tsx
+++ app/layouts/ResultLayout.tsx
@@ -1,13 +1,38 @@
+import DefaultBtn from "@/components/Button/DefaultBtn";
+import FlexView from "@/components/FlexView";
 import Header from "@/components/Header";
+import ResElement from "@/components/ResElement";
+import OnlyText from "@/components/ResElement/OnlyText";
 import { Text, View } from "react-native";
+import styled from "styled-components/native";
 
 const ResultLayout = () => {
   return (
     <View>
       <Header title={"AI 분석 결과"} />
-      <Text>ResultLayout</Text>
+      <ResElementContainer>
+        <FlexView gapVertical={36}>
+          <ResElement title={"쓰레기 분류"}>
+            <Text>Component A</Text>
+          </ResElement>
+          <ResElement title={"직접 수거 여부"}>
+            <Text>Component B</Text>
+          </ResElement>
+          <ResElement title={"수거 자류 개수"}>
+            <Text>Component C</Text>
+          </ResElement>
+          <ResElement title={"담당 지자체"}>
+            <OnlyText content={`제주시 해양수산과\n(Tel. 000-000-0000)`} />
+          </ResElement>
+          <DefaultBtn>보고하기</DefaultBtn>
+        </FlexView>
+      </ResElementContainer>
     </View>
   );
 };
 
 export default ResultLayout;
+
+const ResElementContainer = styled.View`
+  padding: 24px;
+`;
---
+++ components/ResElement/index.tsx
@@ -0,0 +1,28 @@
+import styled from "styled-components/native";
+import { PropsWithChildren } from "react";
+
+interface ResElementProps extends PropsWithChildren {
+  title: string;
+}
+
+const ResElement = ({ title, children }: ResElementProps) => {
+  return (
+    <ElementContainer>
+      <ElementTitle>{title}</ElementTitle>
+      <ElementWrapper>{children}</ElementWrapper>
+    </ElementContainer>
+  );
+};
+
+export default ResElement;
+
+const ElementWrapper = styled.View``;
+
+const ElementTitle = styled.Text`
+  margin: 0 0 16px 0;
+
+  font-family: "ExtraBold";
+  font-size: 32px;
+`;
+
+const ElementContainer = styled.View``;
