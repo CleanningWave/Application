b6ebfd5bb7e2e09157c06c27df23a0b4087adc92
C12H22O12 <director20844@gmail.com>
2024-11-12 20:17:58 +0900

    feat: 수거 자루 개수 표시 레이아웃과 이미지 레이아웃, 스크롤 추가

--- app/layouts/ResultLayout.tsx
+++ app/layouts/ResultLayout.tsx
@@ -2,37 +2,61 @@ import DefaultBtn from "@/components/Button/DefaultBtn";
 import FlexView from "@/components/FlexView";
 import Header from "@/components/Header";
 import ResElement from "@/components/ResElement";
+import CollectInput from "@/components/ResElement/CollectInput";
 import OnlyText from "@/components/ResElement/OnlyText";
 import SelectRadio from "@/components/ResElement/SelectRadio";
-import { Text, View } from "react-native";
+import {
+  Text,
+  ScrollView,
+  Platform,
+  StatusBar,
+  Dimensions,
+} from "react-native";
 import styled from "styled-components/native";
 
 const ResultLayout = () => {
   return (
-    <View>
+    <Container>
       <Header title={"AI 분석 결과"} />
-      <ResElementContainer>
-        <FlexView gapVertical={36}>
-          <ResElement title={"쓰레기 분류"}>
-            <Text>Component A</Text>
-          </ResElement>
-          <ResElement title={"직접 수거 여부"}>
-            <SelectRadio />
-          </ResElement>
-          <ResElement title={"수거 자류 개수"}>
-            <Text>Component C</Text>
-          </ResElement>
-          <ResElement title={"담당 지자체"}>
-            <OnlyText content={`제주시 해양수산과\n(Tel. 000-000-0000)`} />
-          </ResElement>
-          <DefaultBtn>보고하기</DefaultBtn>
-        </FlexView>
-      </ResElementContainer>
-    </View>
+      <ScrollView
+        contentContainerStyle={{ flexGrow: 1 }}
+        showsVerticalScrollIndicator={false}
+      >
+        <ResImage src="https://static01.nyt.com/images/2024/03/05/autossell/00TB-MEOWS/00TB-MEOWS-square640.jpg" />
+        <ResElementContainer>
+          <FlexView gapVertical={36}>
+            <ResElement title={"쓰레기 분류"}>
+              <Text>Component A</Text>
+            </ResElement>
+            <ResElement title={"직접 수거 여부"}>
+              <SelectRadio />
+            </ResElement>
+            <ResElement title={"수거 자루 개수"}>
+              <CollectInput />
+            </ResElement>
+            <ResElement title={"담당 지자체"}>
+              <OnlyText content={`제주시 해양수산과\n(Tel. 000-000-0000)`} />
+            </ResElement>
+            <DefaultBtn>보고하기</DefaultBtn>
+          </FlexView>
+        </ResElementContainer>
+      </ScrollView>
+    </Container>
   );
 };
 
 export default ResultLayout;
+
+const Container = styled.View`
+  padding-top: ${Platform.OS === "android" ? StatusBar.currentHeight : 0}px;
+  background-color: #ffffff;
+  height: ${Dimensions.get("window").height}px;
+`;
+
+const ResImage = styled.Image`
+  width: 100%;
+  height: 340px;
+`;
 
 const ResElementContainer = styled.View`
   padding: 24px;
---
+++ components/ResElement/CollectInput.tsx
@@ -0,0 +1,71 @@
+import styled from "styled-components/native";
+import { Colors } from "@/constants/Colors";
+import DefaultBtn from "../Button/DefaultBtn";
+
+const CollectInput = () => {
+  return (
+    <CollectElementWrapper>
+      <CollectElementContainer>
+        <PairElementContainer>
+          <CollectTextInput />
+          <CollectText>L</CollectText>
+        </PairElementContainer>
+        <PairElementContainer>
+          <CollectTextInput />
+          <CollectText>개</CollectText>
+        </PairElementContainer>
+      </CollectElementContainer>
+      <AddCollectBtnContainer>
+        <AddCollectBtnContents>수거 내역 추가하기</AddCollectBtnContents>
+      </AddCollectBtnContainer>
+    </CollectElementWrapper>
+  );
+};
+
+export default CollectInput;
+
+const AddCollectBtnContents = styled.Text`
+  font-family: "SemiBold";
+  font-size: 24px;
+  color: ${Colors.highlight.highlight_0};
+`;
+
+const AddCollectBtnContainer = styled.TouchableOpacity`
+  align-items: center;
+
+  width: 100%;
+
+  background-color: ${Colors.highlight.highlight_4};
+
+  border-radius: 10px;
+`;
+
+const CollectText = styled.Text`
+  margin-left: 12px;
+
+  font-size: 24px;
+`;
+
+const CollectTextInput = styled.TextInput`
+  width: 93px;
+  height: 48px;
+
+  border: solid 1px ${Colors.neutral.light.light_0};
+  border-radius: 12px;
+`;
+
+const PairElementContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
+
+  margin-right: 20px;
+`;
+
+const CollectElementContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
+
+  margin-bottom: 16px;
+`;
+
+const CollectElementWrapper = styled.View``;
